dcb4251dcd39e07b9f36ddf52f2b69ba6684826a
Tengiz <ti602@nyu.edu>
2024-01-25 16:13:24 +0400

    secondary pca works now

--- module/pca.py
+++ module/pca.py
@@ -28,11 +28,14 @@ class PCA(Data_Processing):
 
     def __init__(self, traces_path, channel = 'C1'):
         super().__init__(traces_path, channel)
-        
-        self.N_CLUSTERS = None
+    
         self.X_compressed = None
-        self.DATA = None
+
         self.pyper = []
+        self.reduced = []
+
+        self.reduced_the_data = []
+        self.X_compressed_red = None
 
 
     def plot_signal(self, voltage,fig=None,ax=None,order=2,mysize=1/50,**kwargs):
@@ -91,7 +94,7 @@ class PCA(Data_Processing):
         
         return (time_under_trig, noize, area,area/std, peakheight, time_at_98_perc, meantime, np.sqrt(np.mean(grad**2)), grad_min, grad_max, np.max(corr))
 
-    def feature_reduction(self):
+    def feature_reduction(self, values_of_interest, a_data):
         """
         This function performs Principal component analysis on the selected functions together with feature reduction and signle value decomposition.
 
@@ -104,7 +107,7 @@ class PCA(Data_Processing):
         """
         
         # Get the data for each of the pulses
-        values      = cp.array([self.values_of_interest(*data[:2],order=2,mysize=1/30) for data in tqdm(self.the_data)])
+        values      = cp.array([values_of_interest(*data[:2],order=2,mysize=1/30) for data in tqdm(a_data)])
 
         # Center data
         mu          = cp.tile(np.mean(values,axis=0),len(values)).reshape(values.shape)
@@ -141,45 +144,52 @@ class PCA(Data_Processing):
         v = vT[k].T
 
         # 3. Calculate the compressed data using np.matmul(), X and stored first k of v^T
-        self.X_compressed = cp.matmul(X_centered,v).get()
+        X_compressed = cp.matmul(X_centered,v).get()
 
         # 4. Print the compressed value of X
         #print(v)
 
         plt.show()
 
+        return X_compressed
+
     ################################### KMeans ###################################
         
     # Initialization Function
-    def initialize_kmeans(self, n_clusters):
+    def initialize_kmeans(self, n_clusters, DATA):
         """
         ----------------------------------------------------------------------------
         
         """
-        kmeans = KMeans(n_clusters=n_clusters, n_init="auto").fit(self.DATA)
+        kmeans = KMeans(n_clusters=n_clusters, n_init="auto").fit(DATA)
         return kmeans
 
     # Plotting Function
-    def plot_clusters(self, kmeans):
+    def plot_clusters(self, kmeans, DATA):
+        """
+        ----------------------------------------------------------------------------
+        
+        """
         sns.set_palette("colorblind")
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
         
         # Create a colormap
         colorblind_palette = sns.color_palette("colorblind")
         cmap = ListedColormap(colorblind_palette)
-        colors = np.array([kmeans.predict([x[:2]])[0] for x in self.DATA])
+        colors = np.array([kmeans.predict([x[:2]])[0] for x in DATA])
         norm = matplotlib.colors.Normalize(vmin=colors.min(), vmax=colors.max())
 
-        points = ax1.scatter(*self.DATA.T, s=10, alpha=0.5, c=cmap(norm(colors)))
+        points = ax1.scatter(*DATA.T, s=10, alpha=0.5, c=cmap(norm(colors)))
         centers = ax1.scatter(*kmeans.cluster_centers_.T, color='k', marker='x', s=50)
         ax1.set_title('Clusters')
         ax2.set_xlabel('Time [ms]')
         ax2.set_ylabel('Voltage [V]')
+
         
         return fig, ax1, ax2, points, centers, colors
 
     # Callback Functions
-    def callback(self, sel, fig, ax2, points, centers, colors, cursor_p, cursor_c):
+    def callback(self, sel, fig, ax2, points, centers, colors, cursor_p, cursor_c, N_CLUSTERS):
         """
         ----------------------------------------------------------------------------
         
@@ -197,11 +207,11 @@ class PCA(Data_Processing):
         ax2.plot(self.REFERENCE_VOLTAGE*self.the_data[I][1].min()/self.REFERENCE_VOLTAGE.min(), color='deepskyblue',lw=1,ls=(0,(5,10)))
         sel.annotation.get_bbox_patch().set(fc="white",lw=0,alpha=0)
         sel.annotation.arrow_patch.set(arrowstyle="]-", fc="k")
-        CC = ['k']*9
+        CC = ['k']*N_CLUSTERS
         CC[colors[I]] = 'deeppink'
         centers.set_color(CC)
 
-    def callback_center(self, sel, fig, ax2, points, centers, colors, cursor_p, cursor_c):
+    def callback_center(self, sel, fig, ax2, points, centers, colors, cursor_p, cursor_c, N_CLUSTERS):
         """
         ----------------------------------------------------------------------------
         
@@ -220,7 +230,7 @@ class PCA(Data_Processing):
         self.pyper.append(idx_c)
         print(len([str(i) for i in idx_c]))
         print((", ".join([str(i) for i in idx_c])))
-        CC = ['k']*9
+        CC = ['k']*N_CLUSTERS
         CC[I] = 'cornflowerblue'
         centers.set_color(CC)
 
@@ -231,21 +241,27 @@ class PCA(Data_Processing):
             self.plot_signal(self.the_data[i][1],fig=fig,ax=ax2,order=2,mysize=1/50,lw=1,label=f'{i}')
 
     # Main Function
-    def KMeans_clustering(self, N_CLUSTERS):
+    def KMeans_clustering(self, N_CLUSTERS, red = False):
         """
         ----------------------------------------------------------------------------
         
         """
-        if self.X_compressed is None : self.feature_reduction()
-        self.DATA = self.X_compressed[:, [0, 1]]
-        kmeans = self.initialize_kmeans(N_CLUSTERS)
-        fig, ax1, ax2, points, centers, colors = self.plot_clusters(kmeans)
+        
+        if not red:
+            if self.X_compressed is None : self.X_compressed = self.feature_reduction(self.values_of_interest, self.the_data)
+
+        else:
+            if self.X_compressed_red is None : self.X_compressed_red = self.secondary_feature_reduction()
+
+        DATA = self.X_compressed[:, [0, 1]]
+        kmeans = self.initialize_kmeans(N_CLUSTERS, DATA)
+        fig, ax1, ax2, points, centers, colors = self.plot_clusters(kmeans, DATA)
         
         cursor_p = mplcursors.cursor(points)
         cursor_c = mplcursors.cursor(centers)
 
-        cursor_p.connect("add", lambda sel: self.callback(sel, fig, ax2, points, centers, colors, cursor_p, cursor_c))
-        cursor_c.connect('add', lambda sel: self.callback_center(sel, fig, ax2, points, centers, colors, cursor_p, cursor_c))
+        cursor_p.connect("add", lambda sel: self.callback(sel, fig, ax2, points, centers, colors, cursor_p, cursor_c, N_CLUSTERS))
+        cursor_c.connect('add', lambda sel: self.callback_center(sel, fig, ax2, points, centers, colors, cursor_p, cursor_c,N_CLUSTERS))
     
     def reset_pyper(self):
         """
@@ -259,3 +275,76 @@ class PCA(Data_Processing):
 
         """
         self.pyper = []
+
+
+    ############################## Secondary KMeans ###################################
+        
+    def reduce_pyper(self):
+        """
+        This function selects points from kmeans plot clicking, on which we want to run secondary pca. 
+        
+        """
+
+        self.reduced.append(self.pyper[1])
+        self.reduced = np.concatenate(self.reduced)
+
+    def reduce_data(self):
+        if len(self.reduced) == 0: self.reduce_pyper()
+        for x in self.reduced:
+            self.reduced_the_data.append(self.the_data[x])
+        
+
+
+    
+    def values_of_interest_red(self, time,data,order=2,mysize=1/50):
+        sos=signal.butter(order,mysize, output='sos')
+        butterybiscuits=signal.sosfilt(sos,data)
+        
+        peak            = np.argmin(butterybiscuits)
+        area            = np.sum(butterybiscuits[:-1]*(time[1:]-time[:-1]))
+        peakheight      = np.min(butterybiscuits)
+        halfheights     = np.where(butterybiscuits-peakheight/2<0)[0]
+        fwhm            = 0 if len(halfheights)<1 else time[halfheights[-1]]-time[halfheights[0]]
+        noize           = len([x for x in data if x>0.0055])
+        medheight       = np.median(butterybiscuits)
+        meanheight      = np.mean(butterybiscuits)
+        meantime        = np.mean(np.abs(butterybiscuits)*(time))/np.sum(np.abs(butterybiscuits[:-1])*(time[1:]-time[:-1]))
+        std             = np.std(butterybiscuits)
+        grad            = (butterybiscuits[1:]-butterybiscuits[:-1])/(time[1:]-time[:-1])
+        grad_min        = np.mean(grad[np.where(grad<0)])
+        grad_max        = np.mean(grad[np.where(grad>0)])
+        corr            = signal.correlate(butterybiscuits/np.min(butterybiscuits)*np.min(self.REFERENCE_VOLTAGE),self.REFERENCE_VOLTAGE)
+        area_cumulative = np.cumsum(butterybiscuits[:-1]*(time[1:]-time[:-1]))
+        cumt_percentage = area_cumulative/area
+        percentage      = 0.96
+        time_at_94_perc = time[np.argmax(cumt_percentage >= percentage)]
+        time_under_trig = len([x for x in butterybiscuits if x >= -0.032])
+        
+        return (peakheight, time_under_trig, time[peak]/meantime ,noize, area/std, time_at_94_perc, np.max(corr))
+    
+    
+    def secondary_feature_reduction(self):
+        """
+        Does what feature_reduction does but with the reduced data instead
+
+        Args:
+        None
+
+        Return:
+        None
+
+        """
+        if len(self.reduced_the_data) == 0:
+            self.reduce_data()
+        self.X_compressed_red = self.feature_reduction(self.values_of_interest_red, self.reduced_the_data)
+
+    
+    def secondary_KMeans_clustering(self, N_CLUSTERS):
+        """
+        This function does Kmean on a smaller subset of selected clusters 
+
+        """
+        self.reduce_data()
+        self.KMeans_clustering(N_CLUSTERS, red=True)
+
+    
